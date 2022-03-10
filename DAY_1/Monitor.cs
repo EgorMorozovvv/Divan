61e0dac8684c887b623bb26d915cfb90c4e90c49
EgorMorozovvv <101330601+EgorMorozovvv@users.noreply.github.com>
2022-03-10 16:29:50 +0300

    Day 1

---
+++ DAY_1/Monitor.cs
@@ -0,0 +1,16 @@
+﻿namespace DAY_1
+{
+    class Monitor
+    {
+        public string Firm;
+        public string Type;
+        public float Diag;
+        public Monitor() { }
+        public Monitor(string Firm, string Type, float Diag)
+        {
+            this.Firm = Firm;
+            this.Type = Type;
+            this.Diag = Diag;
+        }
+    }
+}
---
+++ DAY_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DAY_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Monitor m1 = new Monitor();
+            m1.Firm = "ОАО \"Хлебушек\"";
+            m1.Type = "Прикольный";
+            m1.Diag = 10;
+
+            Monitor m2 = new Monitor("ПГЭК", "Старющий", 5);
+
+            Console.WriteLine($"Монтитор с конструктором по умолчанию:\nФирма: {m1.Firm}, Тип: {m1.Type}, Диагональ: {m1.Diag}.\n");
+            Console.WriteLine($"Монтитор с конструктором с параметрами:\nФирма: {m2.Firm}, Тип: {m2.Type}, Диагональ: {m2.Diag}.");
+
+            Console.ReadKey();
+        }
+    }
+}
